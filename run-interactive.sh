06015d9fbed42e0de1dea30435e7706d8be88bd0
toleksa <toleksa@gmail.com>
2022-05-31 13:09:20 +0200

    fix

--- run-interactive.sh
+++ run-interactive.sh
@@ -3,5 +3,5 @@
 docker stop atk
 docker rm atk
 
-docker run -it -p 8000:8000 --name atk --mount type=bind,source=/home/art/atk/atkdjango,target=/app/atkdjango --mount type=bind,source=/home/art/atk/pics,target=/app/atkdjango/storage/pics atk:latest bash
+docker run -it -p 8000:8000 --name atk --mount type=bind,source=/home/art/atk/atkdjango,target=/app/atkdjango atk:latest bash
 
