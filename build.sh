14dc607dec6835155777d55d8b869a4b5123a19c
toleksa <toleksa@gmail.com>
2022-06-16 09:42:01 +0200

    update v2.0

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-if [ $# -eq 0 ]
-  then
-    TAG='latest'
-  else
-    TAG=$1
-fi
+#
+# ./build.sh [tag] [name]
+#
 
 PWD=$(pwd)
 PROJECT=$(dirname "$(readlink -f "$0")" | gawk -F"/" '{ print $NF }')
 
-docker build -t $PROJECT:$TAG .
+CMD="docker build -t ${2:-$PROJECT}:${1:-latest} ."
+echo $CMD
+eval $CMD
 
