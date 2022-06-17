52eda02f7aeac9d3141739498ee407ba8d937e71
toleksa <toleksa@gmail.com>
2022-06-17 09:44:51 +0200

    new containter name

--- gunicorn/run.sh
+++ gunicorn/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-docker stop gunicorn
-docker rm gunicorn
+docker stop atk-gunicorn
+docker rm atk-gunicorn
 
-docker run -d -p 8000:8000 --name gunicorn --mount type=bind,source=/home/art/atk/atkdjango,target=/app/atkdjango gunicorn:latest
+docker run -d -p 8000:8000 --name atk-gunicorn --mount type=bind,source=/home/art/atk/atkdjango/atk.sqlite,target=/app/atkdjango/atk.sqlite atk-gunicorn:latest
+#docker run -d -p 8000:8000 --name atk-gunicorn atk-gunicorn:latest
+
+
 
