8fb6bf585cad0ac3032a63d9b7ade2754ba8ec00
toleksa <toleksa@gmail.com>
2022-11-07 12:02:24 +0200

    cleanup

--- blog-update.sh
+++ blog-update.sh
@@ -23,25 +23,18 @@ for PAGE in `seq 1 500` ; do
   curl -s "$URL" > index.html
   for GALLERY in `grep posttitle index.html | gawk -F"a href=" '{ print $2 }' | gawk -F"\"" '{ print $2 }'`; do
     curl -s "$GALLERY" > babe.html
-    #MODEL=`grep '<span>Model</span>' babe.html | gawk -F"a href=" '{ print $2 }' | gawk -F"\"" '{ print $2 }'`
     MODEL=`grep '<span>Model</span>' babe.html | gawk -F"<span>Model</span>" '{ print $2 }' | gawk -F"\"" '{ print $2 }'`
-    #NAME=`grep '<span>Model</span>' babe.html | gawk -F"a href=" '{ print $2 }' | gawk -F">" '{ print $2 }' | gawk -F"<" '{ print $1 }' | sed -e "s#'#\"#g" | sed 's/ [0-9]//g'`
     NAME=`grep '<span>Model</span>' babe.html | gawk -F"<span>Model</span>" '{ print $2 }' | gawk -F"[><]" '{ print $3 }' | sed -e "s/'/\"/g"`
     if echo $NAME | grep -e " [0-9]$" > /dev/null; then
       echo "$NAME -> remove digit from name"
       NAME=${NAME::-2}
       echo "new name: $NAME"
     fi
-    #LINK=`grep '<span>Model</span>' babe.html | sed -e 's#<span>Model</span> : ##' | sed -e 's/<br>//g' | sed -e 's/ rel="tag"//g' | sed -e "s#'#\"#g"`
     LINK=`grep '<span>Model</span>' babe.html | gawk -F"<span>Model</span>" '{ print $2 }' | sed -e 's# : ##' | sed -e 's/<br>//g' | sed -e 's/ rel="tag"//g' | sed -e "s#'#\"#g" | gawk -F"<span>" '{ print $1 }'`
-    #DATE=`grep 'postinfo' babe.html | gawk -F">" '{ print $2 }' | gawk -F"," '{ print $1 }'`
     DATE=`grep 'postinfo' babe.html | gawk -F"postinfo\">" '{ print $2 }' | gawk -F"," '{ print $1 }'`
     DATE=$( convert_date $DATE )
-    #AGE=`grep '<span>Age</span>' babe.html | gawk '{ print $3 }' | gawk -F"<" '{ print $1 }'`
     AGE=`grep '<span>Age</span>' babe.html | gawk -F "<span>Age</span> : " '{ print $2 }' | gawk -F"<" '{ print $1 }'`
-    #POB=`grep '<span>Place Of Birth</span>' babe.html | gawk -F":" '{ print $2 }' | gawk -F"<" '{ print $1 }' | sed 's/^ \(.*\)/\1/' | sed -e "s#'#\"#g"`
     POB=`grep '<span>Place Of Birth</span>' babe.html | gawk -F "<span>Place Of Birth</span> : " '{ print $2 }' | gawk -F"<" '{ print $1 }'`
-    #OCC=`grep '<span>Occupation</span>' babe.html | gawk -F":" '{ print $2 }' | gawk -F"<" '{ print $1 }' | sed 's/^ \(.*\)/\1/' | sed -e "s#'#\"#g"`
     OCC=`grep '<span>Occupation</span>' babe.html | gawk -F "<span>Occupation</span> : " '{ print $2 }' | gawk -F"<" '{ print $1 }'`
     TAGS=""
     for TAG in `grep 'Tags:' babe.html | gawk -F"Tags:" '{ print $2 }' | sed -e 's/,/\n/g' | gawk -F"\"" '{ print $2 }' ` ; do TAGS="$TAGS|$TAG" ; done
@@ -67,15 +60,6 @@ for PAGE in `seq 1 500` ; do
     echo "Place: $POB"
     echo "Occupation: $OCC"
     echo "Tags: $TAGS"
-
-    #if [ ! -f "pics/blog/$FILE" ]; then
-    #  wget -O "$FILE" $QUIET "$PIC"
-    #  mv "$FILE" pics/blog
-    #fi
-    #if [ ! -f "pics/blog/$TN" ]; then
-    #  wget -O "$TN" $QUIET "$TN_URL"
-    #  mv "$TN" pics/blog
-    #fi
 
     QUERY=`echo "INSERT INTO atk_babe (id, gallery, name, model, link, date, age, pob, occ, file, tn, tags, likes, monthlikes, duellikes) VALUES ($ID,'$GALLERY','$NAME','$MODEL','$LINK','$DATE','$AGE','$POB','$OCC','$FILE','$TN','$TAGS', 0, 0, 0);" | sed -e "s/''/null/g"`
     echo $QUERY
